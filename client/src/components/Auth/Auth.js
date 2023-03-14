be651b370c2531291cee458f4e8f7679c3dc0a65
qu4ndo111 <qu4ndo111@gmail.com>
2023-03-14 15:05:14 +0700

    modified:   client/src/components/Auth/Auth.js 	modified:   client/src/components/Home/Home.js 	modified:   client/src/index.js

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Avatar, Button, Grid, Typography, Container, Paper } from '@material-ui/core'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import jwt_decode from 'jwt-decode';
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyMiddleware, compose } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 

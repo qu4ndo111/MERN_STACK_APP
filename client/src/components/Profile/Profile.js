7582c62226535bdfd3530380c24078431e99d627
qu4ndo111 <qu4ndo111@gmail.com>
2023-03-20 15:06:32 +0700

    modified:   client/src/components/Profile/Profile.js 	new file:   client/src/components/Profile/styles.js

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,13 +1,37 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { Typography, Avatar, Grid, Grow, AppBar, Button, Container } from '@material-ui/core';
 
+import useStyle from './styles'
+import Posts from '../Posts/Posts';
 
 const Profile = () => {
     const userId = useParams();
-    console.log(userId);
+    const classes = useStyle();
 
     return (
-        <div>Profile</div>
+        <Grow in>
+            <Container maxWidth="xl">
+                <Grid container className={classes.gridContainer}>
+                    <Grid item xs={12} sm={6} md={9}>
+                        <Posts />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} >
+                        <AppBar className={classes.appBarSearch} position='static' color='inherit'>
+                            <div className={classes.profileContainer}>
+                                <img
+                                    src="https://cdn.donmai.us/sample/7a/56/__shirakami_fubuki_sukonbu_fubuzilla_and_fubuchun_hololive_drawn_by_karohroka__sample-7a56b7287c3a4f5f3d6915fe3a034e70.jpg"
+                                    className={classes.profileImage}
+                                />
+                                <div className={classes.avatarContainer}>
+                                    <Avatar className={classes.avatar} sizes="150px" />
+                                </div>
+                            </div>
+                        </AppBar>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
     )
 }
 
---
+++ client/src/components/Profile/styles.js
@@ -0,0 +1,47 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  appBarSearch: {
+    borderRadius: 4,
+    marginBottom: '1rem',
+    display: 'flex',
+    padding: '16px',
+  },
+  pagination: {
+    borderRadius: 4,
+    marginTop: '1rem',
+    padding: '16px',
+  },
+  gridContainer: {
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column-reverse',
+    },
+  },
+  profileContainer: {
+    width: '100%',
+    minHeight: "180px",
+    position: 'relative',
+  },
+  profileImage: {
+    width: '100%',
+    height: '100%',
+  },
+  avatarContainer: {
+    width: '110px',
+    height: '110px',
+    position: 'absolute',
+    bottom: '-10px',
+    left: '0',
+    right: '0',
+    marginInline: 'auto',
+    borderRadius: '50%',
+    backgroundColor: '#FFFFFF',
+    display: "flex",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  avatar: {
+    width: "100px",
+    height: "100px",
+  },
+}));
